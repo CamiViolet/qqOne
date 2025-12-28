e52a4fd6e09e408c9194eb744e6f8994bd927b33
Carlo Camicia <carlo.camicia@auto.tttech.com>
2025-12-28 12:33:03 +0100

    ISSUE-0

--- qqone_bot.py
+++ qqone_bot.py
@@ -1,10 +1,14 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Connessione stabilita con la VPS!")
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
 if __name__ == '__main__':
     app = ApplicationBuilder().token("7952315942:AAEf44ZfD6uRSM3oFieBw3UDPaGPIPESZS8").build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     app.run_polling()
